cd4b29809c0f59edcdcee69bd2a4c674d5d65a75
MadCatCat <95512865+Olixn@users.noreply.github.com>
2025-11-21 10:44:09 +0800

    Update update_rules.sh

--- update_rules.sh
+++ update_rules.sh
@@ -3,13 +3,13 @@
 mkdir -p /tmp/smartdns/
 
 
-wget -O /tmp/smartdns/apple.china.domain.smartdns.conf https://mirror.ghproxy.com/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/apple.china.domain.smartdns.conf
-wget -O /tmp/smartdns/chinalist.domain.smartdns.conf https://mirror.ghproxy.com/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/chinalist.domain.smartdns.conf
-wget -O /tmp/smartdns/gfwlist.domain.smartdns.conf https://mirror.ghproxy.com/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/gfwlist.domain.smartdns.conf
-wget -O /tmp/smartdns/google.china.domain.smartdns.conf https://mirror.ghproxy.com/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/google.china.domain.smartdns.conf
-wget -O /tmp/smartdns/accelerated-domains.china.domain.smartdns.conf https://mirror.ghproxy.com/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/accelerated-domains.china.domain.smartdns.conf
+wget -O /tmp/smartdns/apple.china.domain.smartdns.conf https://cdn.gh-proxy.org/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/apple.china.domain.smartdns.conf
+wget -O /tmp/smartdns/chinalist.domain.smartdns.conf https://cdn.gh-proxy.org/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/chinalist.domain.smartdns.conf
+wget -O /tmp/smartdns/gfwlist.domain.smartdns.conf https://cdn.gh-proxy.org/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/gfwlist.domain.smartdns.conf
+wget -O /tmp/smartdns/google.china.domain.smartdns.conf https://cdn.gh-proxy.org/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/google.china.domain.smartdns.conf
+wget -O /tmp/smartdns/accelerated-domains.china.domain.smartdns.conf https://cdn.gh-proxy.org/https://raw.githubusercontent.com/Olixn/china_list_for_smartdns/main/accelerated-domains.china.domain.smartdns.conf
 # anti-AD 规则
-wget -O /tmp/smartdns/anti-ad.domain.smartdns.conf https://mirror.ghproxy.com/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-smartdns.conf
+wget -O /tmp/smartdns/anti-ad.domain.smartdns.conf https://cdn.gh-proxy.org/https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-smartdns.conf
 
 mv -f /tmp/smartdns/apple.china.domain.smartdns.conf  /etc/smartdns/apple.china.domain.smartdns.conf
 mv -f /tmp/smartdns/chinalist.domain.smartdns.conf  /etc/smartdns/chinalist.domain.smartdns.conf
